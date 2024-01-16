e1147cc21d386b47040850d098954f523b434267
kidjuniper <kidjuniper@yandex.ru>
2024-01-16 16:12:31 +0300

    added unboxing tutorial with animated hand icon & interactive arrows icons: hand icon shows possible swipe directions for moving user puddle, arrows filling with swipes (used layer mask)

---
+++ PongM/PongViewControllerExtensions/PongViewController+TutorialAnimations.swift
@@ -0,0 +1,15 @@
+//
+//  PongViewControllerTutorialAnimations.swift
+//  PongM
+//
+//  Created by Nikita Stepanov on 14.01.2024.
+//
+
+import Foundation
+import UIKit
+
+extension PongViewController {
+    func animateFirstTutorial() {
+        
+    }
+}
